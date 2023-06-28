4602425ec6e12c5a7e3fca5f9a4d5405ddc290ae
Vladimir Efanov <vuefanov@yandex.ru>
2023-06-28 16:00:56 +0300

    fixed a bag in monthsAboveAverage

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -54,7 +54,7 @@ public class StatsService {
         int counter = 0;
         long averageSale = average(sales);
         for (long sale : sales) {
-            if (sale < averageSale) {
+            if (sale > averageSale) {
                 counter++;
             }
         }
